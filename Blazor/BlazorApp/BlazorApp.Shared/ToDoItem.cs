5b8463479bcad006363fb2642ef00e96f543917e
JC <jcmontes95@gmail.com>
2018-05-11 11:15:55 -0300

    initial commit

---
+++ Blazor/BlazorApp/BlazorApp.Shared/ToDoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class ToDoItem
+    {
+		public string Name { get; set; }
+		public string Status { get; set; }
+	}
+}
